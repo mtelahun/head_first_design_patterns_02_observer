6988bace58a1c34c14f5c47af829e7b702131476
Michael Telahun <michael.telahun@dindin.et>
2024-03-24 16:53:50 +0300

    Update main() with forcast display

--- src/main/App.java
+++ src/main/App.java
@@ -3,15 +3,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        ForcastDisplay forcastDisplay = new ForcastDisplay(weatherData);
 
         weatherData.setMeasurements(80f, 65f, 30.4f);
-        String result = currentDisplay.display();
-        System.out.println(result);
+        String resultCurrent = currentDisplay.display();
+        String resultForcast = forcastDisplay.display();
+        System.out.println(resultCurrent);
+        System.out.println(resultForcast);
         weatherData.setMeasurements(82f, 70f, 29.2f);
-        String result2 = currentDisplay.display();
-        System.out.println(result2);
+        String resultCurrent2 = currentDisplay.display();
+        String resultForcast2 = forcastDisplay.display();
+        System.out.println(resultCurrent2);
+        System.out.println(resultForcast2);
         weatherData.setMeasurements(78f, 90f, 29.2f);
-        String result3 = currentDisplay.display();
-        System.out.println(result3);
+        String resultCurrent3 = currentDisplay.display();
+        String resultForcast3 = forcastDisplay.display();
+        System.out.println(resultCurrent3);
+        System.out.println(resultForcast3);
     }
 }
