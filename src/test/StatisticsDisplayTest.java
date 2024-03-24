92f7b258c3b9791c864d51aeaef049d99ea8f422
Michael Telahun <michael.telahun@dindin.et>
2024-03-24 17:08:16 +0300

    test: Avg/Min/Max temperature statistics happy path

--- src/test/StatisticsDisplayTest.java
+++ src/test/StatisticsDisplayTest.java
@@ -26,4 +26,20 @@ public class StatisticsDisplayTest {
         assertEquals((WeatherObserver)display, olist.get(0));
 
     }
+
+    @Test
+    public void givenRegisteredDisplayWith2WeatherMeasurements_WhenNewMeasurement_ThenDisplayAvgMaxMin() {
+        // Arrange
+        WeatherData wd = new WeatherData();
+        StatisticsDisplay display = new StatisticsDisplay(wd);
+        wd.setMeasurements(82.0f, 70f, 29.2f);
+        wd.setMeasurements(61f, 70f, 29.2f);
+
+        // Act
+        wd.setMeasurements(55.5f, 70f, 29.2f);
+
+        // Assert
+        String result = display.display(); 
+        assertEquals("Avg/Max/Min temperature = 66.2/82.0/55.5", result);
+    }    
 }
