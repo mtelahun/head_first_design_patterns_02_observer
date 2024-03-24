db94a9769eaa3b077273566ffaa3c9776fb1cef9
Michael Telahun <michael.telahun@dindin.et>
2024-03-24 16:49:27 +0300

    refactor: forcast display string into its own private method

--- src/main/ForcastDisplay.java
+++ src/main/ForcastDisplay.java
@@ -19,21 +19,27 @@ public class ForcastDisplay implements DisplayElement, WeatherObserver {
 
     @Override
     public String display() {
+        return getForcastString();
+    }
+
+    private String getForcastString() {
         String heading = "Forcast";
+        String msg;
         if (currentPressure > previousPressure) {
-            return String.format(
-                "%s: Improving weather on the way!",
-                heading
-            );
+            msg = String.format(
+                    "%s: Improving weather on the way!",
+                    heading);
         } else if (currentPressure < previousPressure) {
-            return String.format(
+            msg = String.format(
                     "%s: Watch out for cooler, rainy weather",
                     heading);
         } else {
-            return String.format(
+            msg = String.format(
                     "%s: More of the same",
                     heading);
         }
+
+        return msg;
     }
     
 }
