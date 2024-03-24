2e8f430b2a981201f1ed6c956eefc39532116124
Michael Telahun <michael.telahun@dindin.et>
2024-03-24 15:36:38 +0300

    feat: implement Heat Index in current conditions display

--- src/main/CurrentConditionsDisplay.java
+++ src/main/CurrentConditionsDisplay.java
@@ -8,6 +8,7 @@ public class CurrentConditionsDisplay implements DisplayElement, WeatherObserver
     public CurrentConditionsDisplay(WeatherData wd) {
         wd.registerObserver((WeatherObserver)this);
     }
+
     @Override
     public void update(float newTemperature, float newHumidity, float newPressure) {
         temperature = newTemperature;
@@ -18,9 +19,20 @@ public class CurrentConditionsDisplay implements DisplayElement, WeatherObserver
     @Override
     public String display() {
         return String.format(
-            "Current conditions: %.1fF degrees, %.1f%% humidity and %.1fPSI pressure",
-            temperature, humidity, pressure
+            "Current conditions: %.1fF degrees, %.1f%% humidity and %.1fPSI pressure\nHeat index: %.5f",
+            temperature, humidity, pressure, computeHeatIndex(temperature, humidity)
         );
     }
     
-}
+    private float computeHeatIndex(float t, float rh) {
+        float index = (float) ((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) +
+                (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) +
+                (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+                (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+                        (rh * rh * rh))
+                + (0.00000142721 * (t * t * t * rh)) +
+                (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+                0.000000000843296 * (t * t * rh * rh * rh)) -
+                (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+        return index;
+    }}
