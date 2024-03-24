83c795de6bb53d2a1f4486c694114674b2b65d89
Michael Telahun <michael.telahun@dindin.et>
2024-03-24 16:11:13 +0300

    test: Forcast Display object, when instantiated, registers itself as a WeatherData observer

---
+++ src/test/ForcastDisplayTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import main.DisplayElement;
+import main.WeatherData;
+import main.WeatherObserver;
+
+public class ForcastDisplayTest {
+    @Test
+    public void givenDisplayElement_WhenConstructorGivenWeatherData_ThenRegistersItselfAsObserver() {
+        // Arrange
+        WeatherData wd = new WeatherData();
+
+        // Act
+        DisplayElement display = new ForcastDisplay(wd);
+
+        // Assert
+        ArrayList<WeatherObserver> olist = wd.getObservers();
+        assertEquals(1, olist.size());
+        assertEquals((WeatherObserver)display, olist.get(0));
+
+    }
+}
